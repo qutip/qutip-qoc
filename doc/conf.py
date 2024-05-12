84d7ddc0a55c08a2b3d9572f8068410adc50884d
Patrick <>
2024-05-12 11:22:56 +0200

    add module path for autodoc

--- doc/conf.py
+++ doc/conf.py
@@ -14,6 +14,7 @@ import pathlib
 import sys
 
 sys.path.insert(0, os.path.abspath("."))
+sys.path.insert(0, os.path.abspath("../src/qutip_qoc"))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
